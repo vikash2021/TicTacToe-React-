2d37b508024267cd2365809dda907b8176ca3f29
VIKASH <kumarv4624@gmail.com>
2020-05-12 09:52:07 +0530

    commit

--- src/Component/Status.js
+++ src/Component/Status.js
@@ -1,35 +1,26 @@
 import React, { Component } from 'react';
 import Player from  './Component/choosingPlayer';
 
-class Status extends Component
-{
-
-    handleSetPlayer(e){
+class Status extends Component {
+    handleSetPlayer(e) {
         this.props.setPlayer(e)
-
     }
-
-handleReset()
-{
-    this.props.reset();
-}
-
-    renderHtml()
-    {
-        if(this.props.winner){
-
-          return   (<h2>Winner is {this.props.winner}</h2>  )
-        }
-        else{
-            this.props.player?
-            <h2> Next player is{this.props.player}</h2>:
-            <Player player={(e)=>this.handleSetPlayer(e)}/>
+    renderHtml() {
+        if (this.props.winner) {
+            return (<h2>Winner is {this.props.winner}</h2>)
+        } else {
+            return this.props.player ?
+                <h2>Next player is {this.props.player}</h2> :
+                <Player player={(e) => this.handleSetPlayer(e)} />
         }
     }
-
-    render(){
-    return  (<span>{this.renderHtml()}</span>)
+    render() {
+        return (<span>{this.renderHtml()}</span>)
     }
 }
 
-export default Status
+
+
+
+
+export default Status;
--- src/Component/choosingPlayer.js
+++ src/Component/choosingPlayer.js
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
- 
+import React, { Component } from 'react';
 
-class Player extends Component
-{
+class Player extends Component {
 
-    handleForm(e)
-    {
+    handleForm(e) {
         e.preventDefault();
-        console.log(this.props.player(e.target.player.value))
+        this.props.player(e.target.player.value)
     }
-    render(){
-        return (<form onSubmit={(e)=>this.handleForm(e)}>
-                    <label>
-                         Player X
-
-                    <input type="radio" name="Player" value="X"/>
-
-                    </label>
-
-                    <label>
-                        Player O
-
-                        <input type="radio" name="Player" value="O"/>
-
-                    </label>
-                    <input type="submit" value="Start"/>
-
-                </form>
-
+    render() {
+        return (
+            <form onSubmit={(e) => this.handleForm(e)}>
+                <label>
+                    Player X
+                <input type="radio" name="Player" value="X" />
+                </label>
+                <label>
+                    Player O
+                <input type="radio" name="Player" value="O" />
+                </label>
+                <input type="submit" value="Start" />
+            </form>
         )
     }
 }
+
 export default Player;
